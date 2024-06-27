87ca4f037db7a624381539ceb979815071874597
Edita Ginterytė-Šidlauskienė <edita.ginteryte@gmail.com>
2024-06-27 10:50:09 +0300

    Add project files.

---
+++ SolidWorksTankDesign/TaskpaneHostUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace SolidWorksTankDesign
+{
+    //Setting prog id the string in variable SWTASKPANE_PROGID, and then UserControl (below) gets the prog id.
+    // User control will be injected into the SolidWorks by passing this id.
+    [ProgId(TaskpaneIntegration.SWTASKPANE_PROGID)]
+    public partial class TaskpaneHostUI : UserControl
+    {
+        public TaskpaneHostUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
